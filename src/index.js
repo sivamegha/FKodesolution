b72ac96fab8c8b34f4b912caaabc4dcbc39479ee
sivamegha <subbulakshmi771989@gmail.com>
2026-02-09 18:07:49 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <HelmetProvider>
-  <BrowserRouter>
+  <HashRouter >
     <App />
-  </BrowserRouter>
+  </HashRouter >
   </HelmetProvider>
 );
 
